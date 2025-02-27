0cc208633404403dbf9322a9e09e7400124f0af0
liuyue.it@gmail.com <liuyue.it@gmail.com>
2025-02-27 11:43:47 +0000

    react update

---
+++ my-react-app/src/LookBanana.js
@@ -0,0 +1,15 @@
+
+import React from 'react';  
+
+function LookBanana() {
+    return (
+        <div>
+        <h1>LookBanana</h1>
+        <p>Look at this banana!</p>
+        <img src="./lookbananalogo.png" alt="Banana" />
+        <p>This is very first beginning!</p>
+        </div>
+    );
+}
+
+export default LookBanana;
