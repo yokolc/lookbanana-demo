ffa42e32a15296843739aba37c6340783c294cb5
liuyue.it@gmail.com <liuyue.it@gmail.com>
2025-09-24 11:28:31 +0100

    homepage uodate

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+// src/pages/NotFoundPage.js
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>404</h1>
+      <p>Oops! The page you are looking for does not exist.</p>
+    </div>
+  );
+}
